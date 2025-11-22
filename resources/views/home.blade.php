3bb8008a432b76f989fd9b21ebb5881f4dc112da
Christ Eliezer BOUBOUE <christ.b@ezaklinklin.com>
2025-11-22 18:30:58 +0100

    Display full department name on homepage emergency section based on postal code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -247,8 +247,41 @@
                             <i class="fas fa-exclamation-triangle text-2xl md:text-3xl animate-pulse"></i>
                             <h3 class="text-xl md:text-2xl font-black">URGENCE 24/7</h3>
                         </div>
+                        @php
+                            $postalCode = setting('company_postal_code', '78000');
+                            $departmentCode = substr($postalCode, 0, 2);
+                            $departments = [
+                                '01' => 'Ain', '02' => 'Aisne', '03' => 'Allier', '04' => 'Alpes-de-Haute-Provence',
+                                '05' => 'Hautes-Alpes', '06' => 'Alpes-Maritimes', '07' => 'Ardèche', '08' => 'Ardennes',
+                                '09' => 'Ariège', '10' => 'Aube', '11' => 'Aude', '12' => 'Aveyron',
+                                '13' => 'Bouches-du-Rhône', '14' => 'Calvados', '15' => 'Cantal', '16' => 'Charente',
+                                '17' => 'Charente-Maritime', '18' => 'Cher', '19' => 'Corrèze', '21' => 'Côte-d\'Or',
+                                '22' => 'Côtes-d\'Armor', '23' => 'Creuse', '24' => 'Dordogne', '25' => 'Doubs',
+                                '26' => 'Drôme', '27' => 'Eure', '28' => 'Eure-et-Loir', '29' => 'Finistère',
+                                '2A' => 'Corse-du-Sud', '2B' => 'Haute-Corse', '30' => 'Gard', '31' => 'Haute-Garonne',
+                                '32' => 'Gers', '33' => 'Gironde', '34' => 'Hérault', '35' => 'Ille-et-Vilaine',
+                                '36' => 'Indre', '37' => 'Indre-et-Loire', '38' => 'Isère', '39' => 'Jura',
+                                '40' => 'Landes', '41' => 'Loir-et-Cher', '42' => 'Loire', '43' => 'Haute-Loire',
+                                '44' => 'Loire-Atlantique', '45' => 'Loiret', '46' => 'Lot', '47' => 'Lot-et-Garonne',
+                                '48' => 'Lozère', '49' => 'Maine-et-Loire', '50' => 'Manche', '51' => 'Marne',
+                                '52' => 'Haute-Marne', '53' => 'Mayenne', '54' => 'Meurthe-et-Moselle', '55' => 'Meuse',
+                                '56' => 'Morbihan', '57' => 'Moselle', '58' => 'Nièvre', '59' => 'Nord',
+                                '60' => 'Oise', '61' => 'Orne', '62' => 'Pas-de-Calais', '63' => 'Puy-de-Dôme',
+                                '64' => 'Pyrénées-Atlantiques', '65' => 'Hautes-Pyrénées', '66' => 'Pyrénées-Orientales',
+                                '67' => 'Bas-Rhin', '68' => 'Haut-Rhin', '69' => 'Rhône', '70' => 'Haute-Saône',
+                                '71' => 'Saône-et-Loire', '72' => 'Sarthe', '73' => 'Savoie', '74' => 'Haute-Savoie',
+                                '75' => 'Paris', '76' => 'Seine-Maritime', '77' => 'Seine-et-Marne', '78' => 'Yvelines',
+                                '79' => 'Deux-Sèvres', '80' => 'Somme', '81' => 'Tarn', '82' => 'Tarn-et-Garonne',
+                                '83' => 'Var', '84' => 'Vaucluse', '85' => 'Vendée', '86' => 'Vienne',
+                                '87' => 'Haute-Vienne', '88' => 'Vosges', '89' => 'Yonne', '90' => 'Territoire de Belfort',
+                                '91' => 'Essonne', '92' => 'Hauts-de-Seine', '93' => 'Seine-Saint-Denis', '94' => 'Val-de-Marne',
+                                '95' => 'Val-d\'Oise', '971' => 'Guadeloupe', '972' => 'Martinique', '973' => 'Guyane',
+                                '974' => 'La Réunion', '976' => 'Mayotte'
+                            ];
+                            $departmentName = $departments[$departmentCode] ?? $departmentCode;
+                        @endphp
                         <p class="text-red-100 text-sm md:text-base">
-                            Intervention rapide à {{ setting('company_city', 'Versailles') }} ({{ substr(setting('company_postal_code', '78'), 0, 2) }}) • Fuite, dégât des eaux, débouchage
+                            Intervention rapide à <strong class="text-white">{{ setting('company_city', 'Versailles') }}</strong> et tout le département <strong class="text-white">{{ $departmentName }} ({{ $departmentCode }})</strong> • Fuite, dégât des eaux, débouchage
                         </p>
                     </div>
                     
